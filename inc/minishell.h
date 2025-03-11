363f02beba64768beb7b074740ca5cad596358b9
Flash <flash19@mac.home>
2025-03-11 09:15:21 +0100

    ajout de cd

--- inc/minishell.h
+++ inc/minishell.h
@@ -3,11 +3,19 @@
 
 # include "../Libft/inc/libft.h"
 
+typedef struct s_env
+{
+    int				value;
+	char			*name;
+	struct s_env	*next;
+}	t_env;
+
 typedef struct s_data
 {
     char    *input;
 	char	**env;
 	char	*path;
+	
 }	t_data;
 
 // -------------------- Pars --------------------- //
@@ -16,6 +24,7 @@ char	*read_input(t_data *data);
 // -------------------- Exec -------------------- //
 void    exit_clean(t_data *data);
 int		ft_echo(int ac, char **av);
+int		ft_cd(char **av, t_env *env);
 
 // -------------------- Utils -------------------- //
 t_data	*init_data(int ac, char **av, char **env);
