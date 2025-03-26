1784ffcee77600b02a79add106b62584560beff7
lowatell <lowatell@MacBook.lan>
2025-03-26 22:42:21 +0100

    delete useless files

--- src/utils/printf_tab.c
+++ src/utils/printf_tab.c
@@ -6,7 +6,7 @@
 /*   By: lowatell <lowatell@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 16:49:18 by lowatell          #+#    #+#             */
-/*   Updated: 2025/03/11 18:18:03 by lowatell         ###   ########.fr       */
+/*   Updated: 2025/03/26 22:15:15 by lowatell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,5 +23,30 @@ void	print_tab(char **tab)
 	{
 		ft_printf("%s", tab[i]);
 		ft_printf("\n");
+	}
+}
+void	print_cmds(t_cmd *cmds)
+{
+	int		i;
+	t_redir	*redir;
+
+	while (cmds)
+	{
+		ft_printf("=== Command ===\n");
+		i = 0;
+		while (cmds->args && cmds->args[i])
+		{
+			ft_printf("arg[%d]: %s\n", i, cmds->args[i]);
+			i++;
+		}
+		redir = cmds->redir;
+		while (redir)
+		{
+			ft_printf("redir: type = %d, file = %s\n", redir->type, redir->file);
+			redir = redir->next;
+		}
+		cmds = cmds->next;
+		if (cmds)
+			ft_printf("|\n");
 	}
 }
