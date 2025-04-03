5f9cf58daea853fe8c1eeafc623680802d7250ac
lowatell <lowatell@MacBook.lan>
2025-04-03 17:57:21 +0200

    norm .h files

--- inc/token.h
+++ inc/token.h
@@ -6,9 +6,14 @@
 /*   By: lowatell <lowatell@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/02 16:30:22 by lowatell          #+#    #+#             */
-/*   Updated: 2025/04/02 20:20:22 by lowatell         ###   ########.fr       */
+/*   Updated: 2025/04/03 17:56:46 by lowatell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef TOKEN_H
+# define TOKEN_H
+
+# include "minishell.h"
 
 typedef enum e_token_type
 {
@@ -26,3 +31,5 @@ typedef struct s_token
 	t_token_type	type;
 	struct s_token	*next;
 }	t_token;
+
+#endif
